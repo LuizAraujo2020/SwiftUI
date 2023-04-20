fa8098381fa9356487a66cac1f5240c501912579
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-04-20 19:41:20 -0300

    Add files via upload

---
+++ UI/Layout/GeometryReaderInception/GeometryReaderInception/GeometryReaderInceptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderInceptionApp.swift
+//  GeometryReaderInception
+//
+//  Created by Luiz Araujo on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderInceptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
