8dd93dbdde37be76bb6cab2b8b9dfd6b3ef662cd
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-06-17 19:29:49 -0300

    init Tappy Birdy

---
+++ SpriteKit/SKTappyBirdy/SKTappyBirdy/SKTappyBirdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKTappyBirdyApp.swift
+//  SKTappyBirdy
+//
+//  Created by Luiz Araujo on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SKTappyBirdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
